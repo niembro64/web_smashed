bff09705907d5dcf83b77a5fa7c9836c73bc5131
Eric Niemeyer <eric.niemeyer@venturetec.net>
2023-01-29 17:56:38 -0500

    dded file

--- backend/server/models/smashed.model.js
+++ backend/server/models/smashed.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const minLength = 3;
 
@@ -6,19 +6,35 @@ const SmashedSchema = new mongoose.Schema(
   {
     ip: {
       type: String,
-      required: [true, 'IP is required'],
-      minlength: [minLength, 'IP must be at least 3 characters'],
+      required: [true, "IP is required"],
+      minlength: [minLength, "IP must be at least 3 characters"],
     },
     timeStamp: {
       type: String,
-      required: [true, 'Time stamp is required'],
-      minlength: [minLength, 'Time stamp must be at least 3 characters'],
+      required: [true, "Time stamp is required"],
+      minlength: [minLength, "Time stamp must be at least 3 characters"],
+    },
+    city: {
+      type: String,
+      required: [true, "City is required"],
+    },
+    region: {
+      type: String,
+      required: [true, "Region is required"],
+    },
+    country: {
+      type: String,
+      required: [true, "Country is required"],
+    },
+    clientVisits: {
+      type: String,
+      required: [true, "Client visits is required"],
     },
   },
   { timestamps: true }
 );
 
-const Smashed = mongoose.model('Smashed', SmashedSchema);
+const Smashed = mongoose.model("Smashed", SmashedSchema);
 
 module.exports = Smashed;
 
