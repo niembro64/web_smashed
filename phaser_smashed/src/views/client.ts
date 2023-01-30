e7a9e99a765532b799abfff16cb48e8a85166728
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-01-30 00:36:01 -0500

    just gonna do the replace method

--- phaser_smashed/src/views/client.ts
+++ phaser_smashed/src/views/client.ts
@@ -101,22 +101,24 @@ export const saveToAxios = async (
     clientVisits: clientInformation.clientVisits,
   };
 
-  if (process.env.NODE_ENV === 'production') {
-    await axios.post('/api/smashed/create', sessionInfo);
-  } else {
-    await axios.post('http://localhost:8000/api/smashed/create', sessionInfo);
-  }
+  await axios.post('http://localhost:8000/api/smashed/create', sessionInfo);
+  // if (process.env.NODE_ENV === 'production') {
+  //   await axios.post('/api/smashed/create', sessionInfo);
+  // } else {
+  //   await axios.post('http://localhost:8000/api/smashed/create', sessionInfo);
+  // }
   // await axios.post("http://localhost:8000/api/smashed/create", sessionInfo);
   return sessionInfo;
 };
 
 export const getAllAxios = async (): Promise<SessionInfo[]> => {
   let response;
-  if (process.env.NODE_ENV === 'production') {
-    response = await axios.get('/api/smashed');
-  } else {
-    response = await axios.get('http://localhost:8000/api/smashed');
-  }
+  response = await axios.get('http://localhost:8000/api/smashed');
+  // if (process.env.NODE_ENV === 'production') {
+  //   response = await axios.get('/api/smashed');
+  // } else {
+  //   response = await axios.get('http://localhost:8000/api/smashed');
+  // }
   // let response = await axios.get("http://localhost:8000/api/smashed");
   return response.data;
 };
