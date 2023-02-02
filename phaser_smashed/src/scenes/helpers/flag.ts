52252fa499a84a7d0bab7c2af6c0d1cf3075c401
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-02-01 21:44:55 -0500

    sin

--- phaser_smashed/src/scenes/helpers/flag.ts
+++ phaser_smashed/src/scenes/helpers/flag.ts
@@ -3,7 +3,7 @@ import Game from '../Game';
 export const updateFlag = (game: Game): void => {
   let f = game.flag;
 
-  f.yPosition = Math.sin(game.time.now / 1000) * 100 + 200;
+  f.yPosition = Math.sin(game.time.now / 1000);
 
-  game.flag.sprite.y = f.yPosition;
+  game.flag.sprite.y = f.yPosition * 100 + 200;
 };
