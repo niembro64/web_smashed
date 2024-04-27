81c0cb58e9103c46e2fa0fe3774afe39c6d45658
Eric Niemeyer <niemeyer.eric@gmail.com>
2024-04-27 04:28:48 -0400

    clean up

--- phaser_smashed/src/scenes/helpers/keyboard.ts
+++ phaser_smashed/src/scenes/helpers/keyboard.ts
@@ -3,12 +3,8 @@ import { Player } from '../interfaces';
 
 export function updatePadCurrKeyboard(player: Player, game: Game): void {
   if (player.keyboard) {
-    // player.padCurr.up = player.keyboard.up
-
-    let c = player.padCurr;
-    let k = player.keyboard;
-
-    // set all to isDown
+    const c = player.padCurr;
+    const k = player.keyboard;
 
     c.up = k.up.isDown;
     c.down = k.down.isDown;
@@ -25,23 +21,5 @@ export function updatePadCurrKeyboard(player: Player, game: Game): void {
 
     c.start = k.start.isDown;
     c.select = k.select.isDown;
-
-    // set all to !isUp
-
-    // c.up = !k.up.isUp;
-    // c.down = !k.down.isUp;
-    // c.left = !k.left.isUp;
-    // c.right = !k.right.isUp;
-
-    // c.A = !k.A.isUp;
-    // c.B = !k.B.isUp;
-    // c.X = !k.X.isUp;
-    // c.Y = !k.Y.isUp;
-
-    // c.L = !k.L.isUp;
-    // c.R = !k.R.isUp;
-
-    // c.start = !k.start.isUp;
-    // c.select = !k.select.isUp;
   }
 }
